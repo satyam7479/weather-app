492c9f19573451c079fc2487dfdf89274fe965fe
Andrew Ryan <acr197@gmail.com>
2018-03-11 00:34:16 -0500

    Cleaned up JS

---
+++ index.js
@@ -0,0 +1,25 @@
+function multiplyBy() {
+    input = document.getElementById("input").value;
+    document.getElementById("result").innerHTML = input * 1.8 + 32;
+
+var result  = input * 1.8 + 32;
+
+  if (result < 40) {
+    document.body.style.backgroundColor = "lightblue";
+    }
+
+  else if (result >= 40 && result < 60) {
+   document.body.style.backgroundColor = "orange";
+   }
+
+  else if (result >= 60 && result < 80) {
+   document.body.style.backgroundColor = "yellow";
+    }
+
+  else {
+    document.body.style.backgroundColor = "red";
+   }
+  }
+
+
+
